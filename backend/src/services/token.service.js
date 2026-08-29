97af4e5d72821556d0b2f1de745af38a802efa2e
bijaybudha-1 <bijay.budhasp25@cps.edu.np>
2026-08-29 14:06:10 +0545

    add generateTemporaryToken and getHashedToken token services

--- backend/src/services/token.service.js
+++ backend/src/services/token.service.js
@@ -1,12 +1,23 @@
-import { randomBytes } from "node:crypto";
-import bcrypt from "bcrypt";
+import crypto from "node:crypto";
 
-const generateTemporaryToken = async () => {
-  const unHashedToken = randomBytes(20).toString("hex");
-  const hashedToken = await bcrypt.hash(unHashedToken, 10);
+const generateTemporaryToken = () => {
+  const unHashedToken = crypto.randomBytes(20).toString("hex");
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(unHashedToken)
+    .digest("hex");
+
   const tokenExpiry = new Date(Date.now() + 20 * 60 * 1000); // 20min
 
   return { unHashedToken, hashedToken, tokenExpiry };
 };
 
-export { generateTemporaryToken };
+const getHashedToken = (verificationToken) => {
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(verificationToken)
+    .digest("hex");
+
+  return hashedToken;
+};
+export { generateTemporaryToken, getHashedToken };
