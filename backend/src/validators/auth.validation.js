82703e5df09ce4484e1fa308bd7ba28ad9e6eb02
bijaybudha-1 <bijay.budhasp25@cps.edu.np>
2026-08-30 21:47:07 +0545

    add resetPasswordRequestBodySchema auth validation

--- backend/src/validators/auth.validation.js
+++ backend/src/validators/auth.validation.js
@@ -14,8 +14,13 @@ const forgotPasswordRequestBodySchema = z.object({
   email: z.string().trim().email().lowercase(),
 });
 
+const resetPasswordRequestBodySchema = z.object({
+  password: z.string().trim().min(6, "Password must be at least 6 characters"),
+});
+
 export {
   registerPostRequestBodySchema,
   loginPostRequestBodySchema,
   forgotPasswordRequestBodySchema,
+  resetPasswordRequestBodySchema,
 };
