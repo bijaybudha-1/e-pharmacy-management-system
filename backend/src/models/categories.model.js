84530ece43fbfa7cc4810c236ac6f6e6fd850c4d
bijaybudha-1 <bijay.budhasp25@cps.edu.np>
2026-09-01 16:04:58 +0545

    Add categories.model.js

---
+++ backend/src/models/categories.model.js
@@ -0,0 +1,29 @@
+import {
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
+import { AvailableStatus, StatusEnum } from "../utils/constants.js";
+
+export const categoryStatusEnum = pgEnum("status", [
+  StatusEnum.ACTIVE,
+  StatusEnum.INACTIVE,
+]);
+
+const categoriesTable = pgTable("categories", {
+  categoryId: uuid("category_id").primaryKey().defaultRandom(),
+  categoryName: varchar("category_name", { length: 100 }).notNull().unique(),
+  description: text(),
+  categoryStatus: categoryStatusEnum("category_status")
+    .notNull()
+    .default(StatusEnum.ACTIVE),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .$onUpdate(() => new Date())
+    .notNull(),
+});
+
+export default categoriesTable;
