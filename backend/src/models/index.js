19cac8b5c5020563f0e9fc30ea184b5617ae3040
bijaybudha-1 <bijay.budhasp25@cps.edu.np>
2026-09-01 16:12:17 +0545

    Add medicine model and push the database

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,4 +1,15 @@
 import roleTable, { userRoleEnum } from "./role.model.js";
 import userTable, { userStatusEnum } from "./user.model.js";
+import categoriesTable, { categoryStatusEnum } from "./categories.model.js";
+import medicineTable, { medicineStatusEnum } from "./medicine.model.js";
 
-export { userTable, roleTable, userStatusEnum, userRoleEnum };
+export {
+  userTable,
+  userStatusEnum,
+  roleTable,
+  userRoleEnum,
+  categoriesTable,
+  categoryStatusEnum,
+  medicineTable,
+  medicineStatusEnum,
+};
---
+++ backend/src/models/medicine.model.js
@@ -0,0 +1,48 @@
+import {
+  boolean,
+  integer,
+  numeric,
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
+import categoriesTable from "./categories.model.js";
+import {
+  AvailableMedicineStatus,
+  MedicineStatusEnum,
+} from "../utils/constants.js";
+
+export const medicineStatusEnum = pgEnum(
+  "medicine_status",
+  AvailableMedicineStatus,
+);
+
+const medicineTable = pgTable("medicines", {
+  medicineId: uuid().primaryKey().defaultRandom(),
+  categoryId: uuid("category_id")
+    .references(() => categoriesTable.categoryId)
+    .notNull(),
+  medicineName: varchar("medicine_name", { length: 255 }).notNull(),
+  genericName: varchar("generic_name", { length: 150 }),
+  description: text(),
+  form: varchar({ length: 30 }),
+  manufacturer: varchar({ length: 150 }),
+  requiresPrescription: boolean("requires_prescription")
+    .notNull()
+    .default(false),
+  minStock: integer("min_stock").notNull().default(0),
+  sellingPrice: numeric("selling_price", { precision: 10, scale: 2 }),
+  discountPrice: numeric("discount_price", { precision: 10, scale: 2 }),
+  status: medicineStatusEnum("medicine_status")
+    .notNull()
+    .default(MedicineStatusEnum.ACTIVE),
+  createAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .$onUpdate(() => new Date())
+    .notNull(),
+});
+
+export default medicineTable;
