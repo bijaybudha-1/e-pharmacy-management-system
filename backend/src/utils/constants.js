f60d043c87303ec5ffc95a8eb92053713c6500b6
bijaybudha-1 <bijay.budhasp25@cps.edu.np>
2026-09-02 23:31:39 +0545

    Add GenderEnum

--- backend/src/utils/constants.js
+++ backend/src/utils/constants.js
@@ -26,9 +26,16 @@ export const MedicineStatusEnum = {
   DISCONTINUED: "Discontinued",
 };
 
+export const GenderEnum = {
+  MALE: "male",
+  FEMALE: "female",
+  OTHER: "other",
+};
+
 export const AvailableUserRole = Object.values(UserRoleEnum);
 export const AvailablePrescriptionStatus = Object.values(
   PrescriptionStatusEnum,
 );
 export const AvailableStatus = Object.values(StatusEnum);
 export const AvailableMedicineStatus = Object.values(MedicineStatusEnum);
+export const AvailableGender = Object.values(GenderEnum);
