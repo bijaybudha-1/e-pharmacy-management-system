835868ed6f43e0786eea34e4351121fd81a45b19
bijaybudha-1 <bijay.budhasp25@cps.edu.np>
2026-09-04 17:17:27 +0545

    Protected Admin Route for crateCategories and updateCategories

--- backend/src/routes/catalog.routes.js
+++ backend/src/routes/catalog.routes.js
@@ -10,20 +10,27 @@ import {
   createCategoryRequestBodySchema,
   updateCategoryRequestBodySchema,
 } from "../validators/catalog.validation.js";
+import { authorizeRole } from "../middlewares/role.middleware.js";
 const router = Router();
 
+// Public Route
 router.route("/categories").get(getAllCategories);
+
+// Protected Admin Route
 router
   .route("/categories")
   .post(
     authMiddleware,
+    authorizeRole(["admin"]),
     validate(createCategoryRequestBodySchema),
     createCategories,
   );
+
 router
   .route("/categories/:categoryId")
   .patch(
     authMiddleware,
+    authorizeRole(["admin"]),
     validate(updateCategoryRequestBodySchema),
     updateCategory,
   );
